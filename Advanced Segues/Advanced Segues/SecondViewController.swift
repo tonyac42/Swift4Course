ea473e482073f7885bdbff06c7e3cce0c6cefdf5
Tony Coniglio <33562301+tonyac42@users.noreply.github.com>
2018-02-11 21:05:23 -0500

    Add files via upload

---
+++ Advanced Segues/Advanced Segues/SecondViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SecondViewController.swift
+//  Advanced Segues
+//
+//  Created by Tony Coniglio on 12/5/17.
+//  Copyright © 2017 Resin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    var userName = "Tony"
+    var activeRow = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Advanced Segues/Advanced Segues/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Advanced Segues
+//
+//  Created by Tony Coniglio on 12/5/17.
+//  Copyright © 2017 Resin. All rights reserved.
+//
+
+import UIKit
+
+let globalVariable = "Tony"
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var activeRow = 0
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toSecondViewController", sender: nil)
+        activeRow = indexPath.row
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSecondViewController" {
+            let secondViewController = segue.destination as! SecondViewController
+            secondViewController.userName = "TheJefe"
+            secondViewController.activeRow = activeRow
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
